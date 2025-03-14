5f3c0436457fc9c5af1b6859302b48eb376bf472
Naulet Théo <theo.naulet@gmail.com>
2025-03-14 10:38:20 +0100

    feat(article): add article list

---
+++ front/src/app/pages/articles/articles.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ArticleService } from 'src/app/services/article.service';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.scss'],
+})
+export class ArticlesComponent implements OnInit {
+  
+  articles: any[] = [];
+  isDescending: boolean = true; // Par défaut, tri décroissant (du plus récent au plus ancien)
+
+  constructor(private router: Router, private articleService: ArticleService) {}
+
+  ngOnInit() {
+    console.log("ArticlesComponent initialized");
+    this.loadArticles();
+  }
+
+  loadArticles() {
+    const userId = 26; 
+
+    this.articleService.getArticlesBySubscribedThemes(userId).subscribe({
+      next: (response) => {
+        console.log("Articles reçus :", response);
+        this.articles = response; 
+      },
+      error: (error) => {
+        console.error("Erreur lors de la récupération des articles", error);
+      }
+    });
+  }
+
+  sortByDate() {
+    this.articles.sort((a, b) => {
+      return this.isDescending
+        ? new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime() 
+        : new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+    });
+  
+    this.isDescending = !this.isDescending;
+  }
+
+  goToCreateArticle() {
+    this.router.navigate(['/article/create']);
+  }
+}
