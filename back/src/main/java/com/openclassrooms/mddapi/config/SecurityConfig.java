b2f39bcac2d520012de07dfdf019868716f323ca
Naulet Théo <theo.naulet@gmail.com>
2025-03-29 15:22:34 +0100

    feat: authorize swagger routes

--- back/src/main/java/com/openclassrooms/mddapi/config/SecurityConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/config/SecurityConfig.java
@@ -77,7 +77,8 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable()) 
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/register", "/auth/login").permitAll() 
+                .requestMatchers("/auth/register", "/auth/login").permitAll()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
